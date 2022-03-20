cab015861404a6dbf372fc5d4f594413ec0eeb45
bona <bona2324@gmail.com>
2022-03-20 23:11:34 +0900

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `https://bona023.github.io/clonechromeapp/img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
